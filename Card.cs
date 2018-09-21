70c6c1f3d7a78022f2237cfae367f6f268562c60
JongHyunP <43371317+JongHyunP@users.noreply.github.com>
2018-09-21 15:08:31 +0900

    Card의 에셋화

---
+++ Card.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Card", menuName ="Card")]
+public class Card : ScriptableObject {
+
+    public new string name; // 카드 이름
+    public string num; // 카드 번호
+    public string cardcolor; // 카드 색
+    public Sprite artworkF; // 카드 앞면
+    public Sprite artworkB; // 카드 뒷면
+    public bool isHeart; // 선호카드 판별
+    public bool isPick; // 뽑은 카드 true , 안뽑 false
+    public float CardPase = 0; // 0은 초기 // 1은 등장 1번 //2는 재선택 때 // 중복 카드 방지, 재선택 용
+}
