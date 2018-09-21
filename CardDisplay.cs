1e150c75643fbcff1c4ade8bd33efbcf5a3c1ee7
JongHyunP <43371317+JongHyunP@users.noreply.github.com>
2018-09-21 15:11:48 +0900

    카드의 정보표시

---
+++ CardDisplay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CardDisplay : MonoBehaviour {
+
+    public Card card;
+
+    public TextMeshPro m_nameText;
+
+    public Text nameText;
+    public Text numText;
+    public Text colorText;
+
+    public SpriteRenderer artworkImageF;
+    public SpriteRenderer artworkImageB;
+    public GameObject heart;
+
+	void Start () {
+       
+    }
+
+    void Update()
+    {
+        m_nameText.text = card.name;
+        numText.text = card.num;
+        colorText.text = card.cardcolor;
+
+        artworkImageF.sprite = card.artworkF;
+        artworkImageB.sprite = card.artworkB;
+
+        if (card.isHeart)
+        {
+            heart.SetActive(true);
+        }
+        else
+        {
+            heart.SetActive(false); 
+        }
+    }
+}
